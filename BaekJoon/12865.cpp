a9a4dd98da880caaccbe2dbb51d8878f6506d851
euni999 <jeoneunhee322@gmail.com>
2021-05-18 00:37:00 +0900

    baekjoon 12865
    백준 12865 : 평범한 배낭

--- BaekJoon/12865.cpp
+++ BaekJoon/12865.cpp
@@ -11,12 +11,8 @@ int value[110][100101];
 void backpack(int n, int k) {
     for (int i=1; i<=n; i++) {
     	for (int j=1; j<=k; j++) {
-    		if (j >= w[i]) {
-				value[i][j] = max(value[i-1][j], value[i-1][j-w[i]] + v[i]); 
-			}
-			else {
-				value[i][j] = value[i-1][j]; 
-			}
+    		if (j >= w[i]) { value[i][j] = max(value[i-1][j], value[i-1][j-w[i]] + v[i]); }
+			else { value[i][j] = value[i-1][j]; }
 		}
 	}
 	cout << value[n][k] ;
@@ -26,7 +22,7 @@ int main() {
     int n, k;
     cin >> n >> k;
     
-    for (int i=1; i<=n; i++) { cin >> w[i] >> v[i];	}
+    for (int i=1; i<=n; i++) { cin >> w[i] >> v[i]; }
     backpack(n, k);
     
     return 0;
