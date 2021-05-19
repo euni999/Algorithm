5f1c650c6b1e167f6eb48e8bf71ffbc90a89da1b
euni999 <jeoneunhee322@gmail.com>
2021-05-19 23:24:52 +0900

    BaekJoon 9663
    백준 9663 : N_Queen

---
+++ BaekJoon/BaekJoon 9663.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int col[110];
+int N;
+int result = 0;
+
+bool promising(int i)
+{
+    int k=0;
+	
+	while (k<i) {
+		if (col[i] == col[k] || abs(col[i] - col[k]) == i-k)
+			{ return  false; }
+		k++;
+	}
+	return true;
+}
+void queens(int i)
+{
+    if(i == N)
+        result += 1;
+    else {
+        for(int j=0; j<N; j++) {
+            col[i] = j;
+            if(promising(i))
+               {queens(i+1);}
+        }
+    }
+}
+
+int main()
+{
+    cin>>N;
+
+    queens(0);
+
+    cout<< result;
+
+    return 0;
+}
